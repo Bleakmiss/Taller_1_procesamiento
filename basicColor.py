ab2195dcea55cc06f96c73bd932411d602e8d702
Bleakmiss <danieljos_2506@hotmail.com>
2021-08-06 11:42:00 -0500

    hola

--- basicColor.py
+++ basicColor.py
@@ -1,27 +1,34 @@
+#Importado de librerias utilizadas en el codigo
 import cv2
 import os
 import numpy as np
 
+#Declaracion de clase con nombre basicColor
 class basicColor:
 
+    #Metodo constructor donde se guarda la imagen al ingresar los dos parametros requeridos (ruta de imagen y nombre de imagen)
     def __init__(self, path, image_name):
         path_file = os.path.join(path, image_name)
-        self.image=cv2.imread(path_file)
+        self.image=cv2.imread(path_file) #Guardado de imagen
 
+    #Metodo displayProperties donde se mostrara el numero de pixeles de la imagen en unidades de Mega y el numero de canales de la imagen
     def displayProperties(self):
-        pixeles= self.image.size
-        canales=self.image.shape
-        print("El numero de pixeles de las imagenes es: ",(pixeles)/1000000,"M y el numero de canales de la imagen es:",canales)
+        pixeles= self.image.size #Numero de pixeles
+        canales=self.image.shape #Canales de la imagen
+        print("El numero de pixeles de las imagenes es: ",(pixeles)/1000000,"MP y el numero de canales de la imagen es:",canales[2]) #Impresion de las caracteristicas
 
+    #Metodo makeBW para convertir la imagen en blanco y negro
     def makeBW(self):
-        image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-        ret, Ibw_otsu = cv2.threshold(image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        return(Ibw_otsu)
+        image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY) #Conversion de imagen a blanco y negro
+        ret, Ibw_otsu = cv2.threshold(image_gray, 0, 255, cv2.THRESH_OTSU) #Aplicacion de metodo de umbralizacion otsu
+        return(Ibw_otsu) #Se devuelve la imagen en blanco y negro umbralizada
 
+    #Metodo colorize para convertir la imagen con el h ingresado por el usuario
     def colorize(self,h):
-        image_hsv=cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        h1, s, v = cv2.split(image_hsv)
-        h2 = h * np.ones_like(s)
-        image_hsv2 = cv2.merge((h2, s, v))
-        image_hsv_bgr = cv2.cvtColor(image_hsv2, cv2.COLOR_HSV2BGR)
-        return(image_hsv_bgr)
+        image_hsv=cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV) #Conversion de imagen a HSV con H ingresado
+        h1, s, v = cv2.split(image_hsv) #Division de la imagen en cada uno de sus canales
+        h2 = h * np.ones_like(s) # Creacion de vector para H con el valor ingresado
+        image_hsv2 = cv2.merge((h2, s, v)) # Union de los canales para imagen HSV con el H del usuario
+        image_hsv_bgr = cv2.cvtColor(image_hsv2, cv2.COLOR_HSV2BGR) #Conversion de imagen HSV a RGB
+        return(image_hsv_bgr) #Se devuelve la imagen en HSV
+
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+#Este codigo fue realizado por Daniel Barandica para la materia de procesamiento de imagenes y video.
+#El codigo fue diseñado para mostrar las propiedades de una imagen, una version de la imagen a blanco y negro y una version de la imagen con un H especifico
+
+
+#Se importa la libreria cv2 y la clase creada en el codigo basicColor
 import cv2
 from basicColor import basicColor as bc
-#hola
+
+#Se define la condicion para ejecutar solo una vez el codigo
 if __name__ == '__main__':
-    path=input("Ingrese la ruta de la imagen con que quiere trabajar: ")
-    name= input("Ingrese el nombre de la imagen: ")
-    #F:\Descargas
-    #lena.png
-    imagen= bc(path,name)
+    #INCISO B
+    path=input("Ingrese la ruta de la imagen con que quiere trabajar: ") #Ruta de la imagen
+    name= input("Ingrese el nombre de la imagen: ") #Nombre de la imagen
 
-    imagen.displayProperties()
-    cv2.imshow("Imagen a blanco y negro", imagen.makeBW())
+    #INCISO A
+    imagen= bc(path,name) #Creacion de la clase con los dos parametros necesarios
+
+    #INCISO C
+    imagen.displayProperties() #Uso de metodo para mostrar las propiedades de la imagen
+
+    #INCISO D
+    imagen_gray=imagen.makeBW() #Uso de metodo para obtener la version de la imagen en blanco y negro
+    cv2.imshow("Imagen a blanco y negro", imagen_gray) #Visualizacion de imagen en blanco y negro
     cv2.waitKey(0)
-    h= int(input("Ingrese el valor del Hue que desea: "))
 
-    imagen_hsv=imagen.colorize(h)
-
-    cv2.imshow("Imagen a blanco y negro", imagen_hsv)
+    #INCISO E
+    h= int(input("Ingrese el valor del Hue que desea: ")) #Valor de H que ingresa el usuario
+    imagen_hsv=imagen.colorize(h) #Uso de metodo para obtener version de la imagen con H ingresado por el usuario
+    cv2.imshow("Imagen con el h dado", imagen_hsv) #Visualizacion de imagen con H ingresado
     cv2.waitKey(0)
